9b323a798930a0554aa185ef3dbb4ed3da0fe8db
shaked571 <shakedgreenfeld@gmail.com>
2021-06-21 17:33:09 +0300

    some touch up

--- model.py
+++ model.py
@@ -17,18 +17,15 @@ class BiLSTM(nn.Module):
         self.dropout = nn.Dropout(dropout)
 
         self.bilstm = nn.LSTM(input_size=self.embed_dim,
-                            hidden_size=hidden_dim,
-                            num_layers=2,
-                            batch_first=True,
-                            bidirectional=True)
+                              hidden_size=hidden_dim,
+                              num_layers=2,
+                              batch_first=True,
+                              bidirectional=True)
 
     def forward(self, x, x_lens):
         output = self.embedding(x)
         output = self.dropout(output)
         x_packed = pack_padded_sequence(output, x_lens, batch_first=True, enforce_sorted=False)
-        # output = self.relu(output) #TODO need?? read paper
-        # output = output.transpose(0, 1)  # make it (seq_len, batch_size, features)
-        # output, (hidden, cell) = self.bilstm(output.unsqueeze(0))
         output, _ = self.bilstm(x_packed)
         output, _ = pad_packed_sequence(output, batch_first=True)
         return output
@@ -42,9 +39,10 @@ class InnerAttention(nn.Module):
         self.tanh = nn.Tanh()
         self.bilstm = BiLSTM(pre_trained_emb, hidden_dim, dropout)
         self.softmax = nn.Softmax(dim=1)
-        self.w_y = nn.Linear(2 * self.hidden_dim, 2 * self.hidden_dim)
-        self.w_h = nn.Linear(2 * self.hidden_dim, 2 * self.hidden_dim)
-        self.w = nn.Linear(2 * self.hidden_dim, 1)
+        self.lstm_out_dim = 2 * self.hidden_dim
+        self.w_y = nn.Linear(self.lstm_out_dim, self.lstm_out_dim)
+        self.w_h = nn.Linear(self.lstm_out_dim, self.lstm_out_dim)
+        self.w = nn.Linear(self.lstm_out_dim, 1)
 
     def forward(self, x, x_lens):
         y = self.bilstm(x, x_lens)
@@ -54,7 +52,6 @@ class InnerAttention(nn.Module):
         m = self.tanh(self.w_y(y) + self.w_h(r_avg_e_l))
         alpha = self.softmax(self.w(m))
         r_att = torch.bmm(y.permute(0, 2, 1), alpha).permute(0, 2, 1)
-        # r_att = y*alpha
         return r_att
 
 
@@ -69,7 +66,6 @@ class Siamese(nn.Module):
     def forward(self, prem, hyp, prem_lens, hyp_lens):
         prem_atten = self.inner_attention(prem, prem_lens)
         hyp_atten = self.inner_attention(hyp, hyp_lens)
-
         mult_vec = prem_atten * hyp_atten
         diff_vec = prem_atten - hyp_atten
         concat_vec = torch.cat([prem_atten, mult_vec, diff_vec, hyp_atten], dim=2)
