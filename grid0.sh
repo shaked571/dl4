965a02d7ce4642f63cfb722b4e4d5e02713c214c
shaked571 <shakedgreenfeld@gmail.com>
2021-06-22 19:55:54 +0300

    fix flag

--- grid0.sh
+++ grid0.sh
@@ -3,6 +3,6 @@ for lr in 0.001 0.002 0.0001
 do
 	for ba in 16 32 64 128 256
 	do
-       	python main.py -s 0 --gpu 0 -x -a -le -ld -b $ba -lr $lr
+       	python main.py -s 0 --gpu 0 -x -a -le -ld -b $ba -l $lr
 	done
 done
