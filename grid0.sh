55b27f77594ac42bed7ae2669fde25d48dfb6367
shaked571 <shakedgreenfeld@gmail.com>
2021-06-22 19:55:25 +0300

    gp crazy on epochs

--- grid0.sh
+++ grid0.sh
@@ -1,8 +1,8 @@
-# GPU 4
+# GPU 0
 for lr in 0.001 0.002 0.0001
 do
 	for ba in 16 32 64 128 256
 	do
-       	python main.py -s 4 --gpu 4 -x -a -le -ld -b $ba -lr $lr
+       	python main.py -s 0 --gpu 0 -x -a -le -ld -b $ba -lr $lr
 	done
 done
