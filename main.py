b1a4a81204fe44dea326ba93909d8eeec68e6391
shaked571 <shakedgreenfeld@gmail.com>
2021-06-22 20:03:49 +0300

    typr fix

--- main.py
+++ main.py
@@ -164,11 +164,11 @@ if __name__ == '__main__':
     parser.add_argument('-a', '--adamw', help='to use adamW instead of RMSprop ', action='store_true')
     parser.add_argument('-e', '--epoch', help='embedding dropout', type=int,default=25, required=False)
     parser.add_argument('-s', '--seed', help='seed', type=int,default=1, required=False)
-    parser.add_argument('-l', '--lr', help='learning rate', type=int,default=0.001, required=False)
+    parser.add_argument('-l', '--lr', help='learning rate', type=float,default=0.001, required=False)
     parser.add_argument('-b', '--batch', help='train batch size', type=int,default=128, required=False)
     parser.add_argument('--gpu', type=int, default=0, required=False)
     args = parser.parse_args()
-
+    print(args.lr)
     set_seed(args.seed)
     trainer = Trainer(args.lstm_drop, args.lstm_embedding, args.xavier, args.adamw,n_ep=args.epoch,
                       gpu=args.gpu,batch=args.batch, lr=args.lr, seed=args.seed)
